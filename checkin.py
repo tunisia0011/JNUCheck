c264d50ec3cfeef57478d097e2c9b883c2ec3b8e
tunisia0011 <82506259+tunisia0011@users.noreply.github.com>
2021-09-26 05:51:20 +0800

    update

--- checkin.py
+++ checkin.py
@@ -112,14 +112,10 @@ class CrackSlider():
         ActionChains(self.driver).move_by_offset(xoffset=3, yoffset=0).perform()
         time.sleep(0.5)
         ActionChains(self.driver).release().perform()
-        try:
-            failure = self.wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, 'yidun_tips__text'), '向右滑动滑块填充拼图'))
-            print(failure)
-        except:
-            print('验证成功')
-            return None
-        if failure:
+        if (self.driver.find_element_by_xpath('//*[@class="yidun_tips__text yidun-fallback__tip"]').get_attribute("innerHTML") == '向右滑动滑块填充拼图'):
             self.crack_slider()
+        else:
+            print('滑动验证通过')
 
 def tg_push(text):
     push_url = 'https://api.telegram.org/bot' + BOTTOKEN + '/sendMessage?chat_id=' + TGCHATID + '&text=' + text
