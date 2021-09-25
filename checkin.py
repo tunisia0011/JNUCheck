1348dc17bf662a146a0c2bc62bc17a05cfe1a5f2
tunisia0011 <82506259+tunisia0011@users.noreply.github.com>
2021-09-26 05:52:43 +0800

    update

--- checkin.py
+++ checkin.py
@@ -19,7 +19,7 @@ class CrackSlider():
     def __init__(self):
         super(CrackSlider, self).__init__()
         chrome_options = webdriver.ChromeOptions()
-        '''chrome_options.add_argument('--headless')'''
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--disable-dev-shm-usage')
